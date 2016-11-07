b4fa6655f21a74faea2292dbf196918365fefc8e
Alisher Sadikov <alishersadikov@gmailcom>
2016-11-06 20:51:35 -0700

    complete signup feature with tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      redirect_to @user
+      flash[:success] = "Welcome to the Sample App!"
     else
       render :new
     end
@@ -21,5 +22,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-    end 
+    end
 end
